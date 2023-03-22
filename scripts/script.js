dcf52ccc910410cbbd4390bd753d368b1ef2cc93
Fahd Fady <55513802+fahdfady@users.noreply.github.com>
2023-03-22 23:00:26 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -61,18 +61,3 @@ for (let i = 0; i < navLink.length; i++) {
 
 };
 
-$('.owl-carousel').owlCarousel({
-    loop: true,
-    margin: 10,
-    responsive: {
-        0: {
-            items: 1
-        },
-        600: {
-            items: 3
-        },
-        1000: {
-            items: 5
-        }
-    }
-})
